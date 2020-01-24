7ae216d1fc2f8ef62a1da8ad7e75c266edb3877c
Fabio Dias <fabiowebmain@gmail.com>
2020-01-24 20:37:42 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'test-pelando';
+}
