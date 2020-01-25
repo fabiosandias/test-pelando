3021d75f7e69e38f69c28e54c37a9c42311c6bbc
Fabio Dias <fabiowebmain@gmail.com>
2020-01-25 00:33:47 -0300

    Criação de components header, filter, home, video-card e divider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { AddVideoComponent } from './components/add-video/add-video.component';
+import { DividerComponent } from './components/divider/divider.component';
+import { VideoCardComponent } from './components/video-card/video-card.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FilterComponent,
+    AddVideoComponent,
+    DividerComponent,
+    VideoCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
